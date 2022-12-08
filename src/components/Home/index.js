5e2a4570fe818432a578e9682fdc17b31f7a3fa1
mounikakancharla96 <kancharlamounika17@gmail.com>
2022-12-08 08:11:46 +0000

    navbar with context

---
+++ src/components/Home/index.js
@@ -0,0 +1,33 @@
+import ThemeContext from '../../context/ThemeContext'
+import Navbar from '../Navbar'
+import './index.css'
+
+const Home = () => (
+  <ThemeContext>
+    {value => {
+      const {isDarkTheme} = value
+
+      const homeBgClassName = isDarkTheme ? 'home-dark-bg' : 'home-white-bg'
+
+      const homeImageUrl = isDarkTheme
+        ? 'https://assets.ccbp.in/frontend/react-js/home-dark-img.png'
+        : 'https://assets.ccbp.in/frontend/react-js/home-light-img.png'
+
+      const homeHeadingClassName = isDarkTheme
+        ? 'white-heading-color'
+        : 'dark-heading-color'
+
+      return (
+        <>
+          <Navbar />
+          <div className={`home-container ${homeBgClassName}`}>
+            <img src={homeImageUrl} alt="home" className="home-image" />
+            <h1 className={`home-heading ${homeHeadingClassName}`}>Home</h1>
+          </div>
+        </>
+      )
+    }}
+  </ThemeContext>
+)
+
+export default Home
